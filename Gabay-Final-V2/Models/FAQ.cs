97c58cd50c06bb74436a2e489a41fd5855973e2e
Jeremiah James <rodriguezjeremiah3k@.com>
2023-10-10 21:22:29 +0800

    FAQ 1st Attemps

---
+++ Gabay-Final-V2/Models/FAQ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Gabay_Final_V2.Models
+{
+    public class FAQ
+    {
+        public int FAQID { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
